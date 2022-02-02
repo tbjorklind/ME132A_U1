98c01176e10933d1bba2b79a1592f017634cf8fa
tbjorklind <97900554+tbjorklind@users.noreply.github.com>
2022-02-02 13:53:20 +0100

    Added code to part 3, U1

--- 3) Function that calculates the average of the numbers in an array/index.js
+++ 3) Function that calculates the average of the numbers in an array/index.js
@@ -44,11 +44,39 @@ VIDEO:  Record a video where you explain how averg works line by line. Max 3 min
 
 */
 
-// Copy your earlier function here
-function adder ( _array ) {}
+// // Copy your earlier function here
+// function adder ( _array ) {}
 
-function averg ( _array ) {
-  // in here you must use adder
+let a1 = [1, 2, 3, 4, 5];
+
+function adder (_arr) {
+  let summ = 0;
+  for (let i = 0 ; i < _arr.length ; i++){
+    summ = summ +_arr[i];
+  }
+  return summ;
+
 }
+console.log (adder(a1))
+
+// function averg ( _array ) {
+//   // in here you must use adder
+// }
+  function averg (_arr) {
+    let sum = adder(a1) / _arr.length;
+
+    return sum;
+  }
+  console.log (averg(a1))
+
+// function averg (_arr) {
+//   let sum = 0;
+//   for (let i = 0 ; i < _arr.length ; i++){
+//     sum = sum +_arr[i] / _arr.length;
+//   }
+//   return sum;
+
+// }
+// console.log (averg(b1))
 
 
