52e0f2765ea89ce77d772083c861be18aa5f7252
tbjorklind <97900554+tbjorklind@users.noreply.github.com>
2022-02-02 09:43:05 +0100

    Collected all the files in one folder

---
+++ 4c) Function that creates a grid from values in input fields/index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+
+/*
+
+We will continue with our work from 4b.
+We need to fix these two things:
+
+First:
+gridMaker must be called each time the user clicks on the button (See index.html).
+
+
+Second:
+We now need to make sure that when we call (anropar) gridMaker we must use
+the values in #inputRows and #inputColumns as arguments.
+
+*/
+
---
+++ 4d) Fill the grid - createNumberDiv/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+
+/*
+
+Code a function createNumberDiv that creates a div, assigns a random number (0-99)
+as the HTML-content of that div (innerHTML) and returns the reference to the
+newly created div.
+
+Note that the newly created div must not be appended inside the function createNumberDiv.
+The must happen outside the function.
+
+
+TEST:
+document.querySelector("body").appendChild( createNumberDiv() )
+This must append a div with a random number to the body each time it is called.
+
+
+*/
