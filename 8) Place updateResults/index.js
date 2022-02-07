b1f8379f2ebecec1b79095ea2f4f6d049b04ecee
tbjorklind <97900554+tbjorklind@users.noreply.github.com>
2022-02-07 13:54:41 +0100

    Added code to part 8, U1

--- 8) Place updateResults/index.js
+++ 8) Place updateResults/index.js
@@ -26,7 +26,43 @@ VIDEO:  Record a video where you explain where you have placed all the lines
 
 */
 
+"use strict";
 
+function gridMaker (gridContainer, R, C) {
+
+  gridContainer.style.display ="grid";
+  gridContainer.style["gridTemplateRows"] = `repeat(${R}, 1fr)`;
+  gridContainer.style["gridTemplateColumns"] = `repeat(${C}, 1fr)`;
+
+  gridContainer.innerHTML = "";
+
+  document.querySelector("#selected span").innerHTML = "";
+  document.querySelector("#amount span").innerHTML = "";
+  document.querySelector("#sum span").innerHTML = "";
+  document.querySelector("#average span").innerHTML = "";
+
+  for ( let i = 0; i < C; i++ ) {
+          for ( let ii = 0; ii < R; ii++ ) {
+                  gridContainer.appendChild(createNumberDiv ());
+  };
+  };
+}
+
+document.querySelector ("button").addEventListener ("click", function () {
+  gridMaker (
+      document.querySelector("#grid"),
+      document.getElementById ("inputRows").value,
+      document.getElementById ("inputCols").value
+  )
+});
+
+document.onload = gridMaker(document.querySelector("#grid"), 
+document.querySelector("#inputRows").value, 
+document.querySelector("#inputCols").value);
+
+window.onload = gridMaker(document.querySelector("#grid"),
+document.querySelector("#inputRows").value,
+document.querySelector("#inputCols").value);
 
 /*
 
@@ -72,6 +108,62 @@ function getArrayOfSelectedNumbers (className) {
 
 }
 
+
+function roundString(numberWithManyDecimals, decimals){
+  // From: https://stackoverflow.com/a/12698296/2027283
+  var rounded = Math.pow(10, decimals);
+  return (Math.round(numberWithManyDecimals * rounded) / rounded).toFixed(decimals);
+}
+
+function createNumberDiv () {
+  let divs = document.createElement ("div");
+  divs.innerHTML = Math.floor( 99 * Math.random ());
+  divs.addEventListener ("click", function () {
+  divs.classList.toggle ("selected");
+
+divs.addEventListener ("click", updateResults ("selected"))
+});
+  return divs;
+};
+
+function adder (_array) {
+  let sum = 0;
+  for (let i = 0 ; i < _array.length ; i++){
+    sum = sum +_array[i];
+  }
+  return sum;
+}
+
+  function averg (_arr) {
+    let average = adder(_arr) / _arr.length;
+    average = roundString(average, 1);
+    return average;
+  }
+
+function updateResults (className) {
+
+
+  let array = getArrayOfSelectedNumbers(className);
+
+  let selected = array.join(", ");
+
+
+  let amount = array.length;
+  let sum = adder(array);
+  let average = roundString(averg(array), 1);
+
+  document.querySelector("#selected span").innerHTML = selected;
+  document.querySelector("#amount span").innerHTML = amount;
+  document.querySelector("#sum span").innerHTML = sum;
+  document.querySelector("#average span").innerHTML = average;
+
+}
+
+
+
+
+console.log( [1,3,4,10,0,1].join() );
+console.log( [1,3,4,10,0,1].join(" - ") );
 
 /*
 
@@ -150,10 +242,3 @@ below to always show a number that has one decimal.
 
 
 */
-
-function roundString(numberWithManyDecimals, decimals){
-  // From: https://stackoverflow.com/a/12698296/2027283
-  var rounded = Math.pow(10, decimals);
-  return (Math.round(numberWithManyDecimals * rounded) / rounded).toFixed(decimals);
-}
-
